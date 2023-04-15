f35fff45cb51dc7ac9437d83570ab2f19c8970f8
anka335 <joanna.zubik@uczen.zsk.poznan.pl>
2023-04-15 22:01:23 +0200

    .

--- laravel-test/react/src/components/GuestLayout.jsx
+++ laravel-test/react/src/components/GuestLayout.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import { Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
 import LeftBar from "./GuestLayout/LeftBar";
+import Logo from "../assets/LOGO_GRAJCOWNIA.png";
 
 export default function MainGuestPage(){
     const {token} = useStateContext()
@@ -9,8 +10,10 @@ export default function MainGuestPage(){
     if (token) {
         return <Navigate to="/" />
     }
+
     return(
         <div>
+            <img src={Logo} height={50} />
             <Link to="/login">zaloguj sie</Link>
             <Link to="/signup">zarejestruj sie</Link>
             naglowek
