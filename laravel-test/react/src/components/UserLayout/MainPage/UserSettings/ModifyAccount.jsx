56a062c2753428066b34655373a69b5eadd92537
anka335 <joanna.zubik@uczen.zsk.poznan.pl>
2023-04-22 19:34:45 +0200

    .

--- laravel-test/react/src/components/UserLayout/MainPage/UserSettings/ModifyAccount.jsx
+++ laravel-test/react/src/components/UserLayout/MainPage/UserSettings/ModifyAccount.jsx
@@ -1,10 +1,10 @@
 export default function ModifyAccount(){
     return(
-        <div>
-            <form class="form-inline"><label>zmien haslo</label><input placeholder="stare"/><input placeholder="nowe"/><input type="button"/></form>
-            <p>zmien email</p>
-            <p>zmien nazwe uzytkownika</p>
-            <p>usun konto</p>
+        <div id="ModifyAccount">
+            <div className=".DivesModifyAccount"><span>zmień hasło</span><form class="form-inline"><input placeholder="poprzednie hasło"/><input placeholder="nowe hasło"/><input type="button" value="wyślij" id="ModifyAccountBtn"/></form></div>
+            <div className=".DivesModifyAccount"><span>zmień email</span><form class="form-inline"><input placeholder="hasło"/><input placeholder="nowy email"/><input type="button" value="wyślij" id="ModifyAccountBtn"/></form></div>
+            <div className=".DivesModifyAccount"><span>zmień nazwę użytkownika</span><form class="form-inline"><input placeholder="hasło"/><input placeholder="nowa nazwa użytkownika"/><input type="button" value="wyślij" id="ModifyAccountBtn"/></form></div>
+            <div className=".DivesModifyAccount"><span>usuń konto</span><form class="form-inline"><input placeholder="hasło"/><input type="button" value="wyślij" id="ModifyAccountBtn"/></form></div>
         </div>
     )    
 }
--- laravel-test/react/src/contexts/ContextProvider.jsx
+++ laravel-test/react/src/contexts/ContextProvider.jsx
@@ -11,7 +11,7 @@ export const ContextProvider = ({children}) => {
     const [user, setUser] = useState({
         name: 'John'
     });
-    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN')); //localStorage.getItem('ACCESS_TOKEN')
+    const [token, _setToken] = useState(2); //localStorage.getItem('ACCESS_TOKEN')
     const setToken = (token) => {
         _setToken(token)
         if (token) {
