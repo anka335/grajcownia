d1c7ff96b4361499fe1781ffec88211551eef412
Jeziorop <J._Kolski@wp.pl>
2023-04-27 19:22:56 +0200

    plansze do statków

--- laravel-test/react/src/views/Battleship.jsx
+++ laravel-test/react/src/views/Battleship.jsx
@@ -11,30 +11,34 @@ export default function Battleship(){
                         odlozone statki
                     </div>
                 </aside>
-                <main className="battleship_main">
-                    <div className="battleship_row">1<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row">2<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row">3<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row">4<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row">5<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row">6<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row">7<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row">8<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row">9<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row">10<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                </main>
-                <main className="battleship_main">
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                    <div className="battleship_row"> <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div> </div>
-                </main>
+                <table id="battleShip">
+                    <tbody className="battleShip_main">
+                        <tr className="battleship_labels"><th className="file"></th><th className="file">A</th><th className="file">B</th><th className="file">C</th><th className="file">D</th><th className="file">E</th><th className="file">F</th><th className="file">G</th><th className="file">H</th><th className="file">I</th><th className="file">J</th></tr>
+                        <tr className="battleship_row"><th>1</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>2</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>3</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>4</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>5</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>6</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>7</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>8</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>9</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>10</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                    </tbody>
+                    <tbody className="battleShip_main">
+                        <tr className="battleship_labels"><th className="file"></th><th className="file">A</th><th className="file">B</th><th className="file">C</th><th className="file">D</th><th className="file">E</th><th className="file">F</th><th className="file">G</th><th className="file">H</th><th className="file">I</th><th className="file">J</th></tr>
+                        <tr className="battleship_row"><th>1</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>2</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>3</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>4</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>5</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>6</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>7</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>8</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>9</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                        <tr className="battleship_row"><th>10</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+                    </tbody>
+                </table>
                 <div id="game_empty"></div>
                 <Chat />
             </section>
