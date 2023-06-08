5b028e7ce80b5bb28b126bcd8e24c0cd9f25368d
anka335 <joanna.zubik@uczen.zsk.poznan.pl>
2023-06-08 13:39:18 +0200

    .

--- laravel-test/react/src/components/Default/Games/WordleRooms.jsx
+++ laravel-test/react/src/components/Default/Games/WordleRooms.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import axios from "axios";
 import React from "react";
 import { useEffect } from "react";
-import { Navigate } from "react-router-dom";
 import { StyledLink } from "../../../NavStyle";
 import styled from "styled-components";
 
